765881e2ba1a5555d30755ac4ec95092bbfd641e
Jiang Chunhui <jiangchunhui007@gmail.com>
2019-11-01 11:47:21 +0800

    add timer and python script to process file

--- main.cpp
+++ main.cpp
@@ -2,22 +2,24 @@
 #include <queue>
 #include <cstring>
 #include <vector>
+#include <chrono>
+using namespace std;
 
 typedef vector<int> vi;
 
-using namespace std;
 int w[4010], cp_w[4010];
 int src[600010], des[600010];
 int counter[600010] = {0};
 vector<vi> AL;
 int optimal;
 bool choose[4010] = {0};
+int V, E;
 
 void getNeighbour() {
 
 }
 
-void initial(int V, int E) {
+void initial() {
 	for (int i = 0; i < E; i++) {
 		int small = cp_w[src[i]] <= cp_w[des[i]] ? src[i] : des[i];
 		int e = cp_w[small];
@@ -37,8 +39,9 @@ void initial(int V, int E) {
 }
 
 int main() {
-	//freopen("input.txt", "r", stdin);
-	int V, E;
+	chrono::time_point<chrono::steady_clock> start, end;
+	start = chrono::steady_clock::now();
+	// freopen("input.txt", "r", stdin);
 	scanf("%d%d", &V, &E);
 	AL.assign(V, vi());
 	for (int i = 0; i < V; i++) {
@@ -50,7 +53,16 @@ int main() {
 		AL[src[i]].emplace_back(i);
 		AL[des[i]].emplace_back(i);
 	}
-	initial(V, E);
-	
+	initial();
+	while (1) {
+		// Search
+		end = chrono::steady_clock::now();
+		if (((chrono::duration<double>)(end-start)).count() >= 1.99) break;
+	}
+	printf("%d\n", optimal);
+	for (int i = 0; i < V; i++) {
+		if (choose[i]) printf("%d ", i);
+	}
+	printf("\n");
 	return 0;
 }
