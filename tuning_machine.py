4857adb9b0e6e83b1dc4ec3b0aa8a8ee28f1fb3a
Jiang Chunhui <jiangchunhui007@gmail.com>
2019-11-11 11:25:41 +0800

    update

--- tuning_machine.py
+++ tuning_machine.py
@@ -52,8 +52,8 @@ for alpha in range(2, 21, 5):
         for raw_end in range(3, 21, 5):
             total_median = 0
             end = raw_end * 0.5
-            if (end > start):
-                end = start
+            # if (end > start):
+            #     end = start
             for id in range(len(dataset)):
                 results = []
                 for repeat in range(repeat_times):
